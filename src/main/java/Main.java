d96f59c17e3cca3108764fd5861eb09e8757aec1
JHenrique <henryque.pc.game@gmail.com>
2025-07-02 21:24:38 -0300

    comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        // o polimofismo, o animal sera para qual quer tipo for animal
         Animal bicho = new Animal("O Bicho ");
         bicho.fazSom();
 
+        // a classe herança do cachorro vem do animal
         Animal cachorro = new Cachorro("Cachorro");
         cachorro.fazSom();
     }
